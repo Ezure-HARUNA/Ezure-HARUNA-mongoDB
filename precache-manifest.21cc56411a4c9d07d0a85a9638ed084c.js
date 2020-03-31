52dea65eb6fa78dbf7ec03bf7a4e007c77a8a288
ezure haruna <burimofumofu@gmail.com>
2020-03-31 13:22:33 +0900

    Updates

---
+++ precache-manifest.21cc56411a4c9d07d0a85a9638ed084c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b89c46819c556740478a47c43b75d08",
+    "url": "/Movie-App/index.html"
+  },
+  {
+    "revision": "7819188bc54d205d711a",
+    "url": "/Movie-App/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "508e880db1327d00c52c",
+    "url": "/Movie-App/static/js/2.0420a6d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-App/static/js/2.0420a6d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7819188bc54d205d711a",
+    "url": "/Movie-App/static/js/main.ab71f69d.chunk.js"
+  },
+  {
+    "revision": "a1b04e68c5580de1bb33",
+    "url": "/Movie-App/static/js/runtime-main.3523c3cb.js"
+  }
+]);
